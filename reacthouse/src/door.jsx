2154c1af08c5109a657da6a46097ee4638f1273a
Preethi <preethimadhu0106@gmail.com>
2025-11-21 09:51:26 +0530

    second commit

--- reacthouse/src/door.jsx
+++ reacthouse/src/door.jsx
@@ -2,22 +2,29 @@ import Grid from '@mui/material/Grid'
 import Item from '@mui/material/Grid'
 import { useState } from "react"
 function Door(){
-    const [isOpen , setIsOpen] = useState(false);
-    if(isOpen){
+    const [isOpen , setIsOpen] = useState(false);////Using dynamic status
+    // if(isOpen){
         return(
-        <Grid container  sx={{width:'110px',height:'160px', border:'1px solid black', bgcolor:'#5cbeaaff'}}>
+            <>
+        <button onClick={() => setIsOpen(!isOpen)}>
+        {isOpen ? "Close Door" : "Open Door"}
+      <Grid container  sx={{width:'110px',height:'160px', border:'1px solid black', bgcolor:'#5cbeaaff'}}>
                     <Grid size={'grow'} sx={{border:'0.5px solid black'}}></Grid>
                     <Grid size={'grow'} sx={{border:'0.5px solid black'}}></Grid>
-        </Grid>
+        </Grid></button>
+       
+        </>
+      
+    
         );
-    }
-    else{
+    // }
+    // else{
         return(
         <Grid container  sx={{width:'110px',height:'160px', border:'1px solid black', bgcolor:'#5cbeaaff'}}>
-                    <Grid size={'grow'} sx={{border:'0.5px solid black'}}>opened</Grid>
+                    <Grid size={'grow'} sx={{border:'0.5px solid black'}}>{isOpen ? 'opened' : 'closed'}</Grid>
                     <Grid size={'grow'} sx={{border:'0.5px solid black'}}></Grid>
         </Grid>
     );
-    }
+    // }
 }
 export default Door
